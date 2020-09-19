1f6f371bb1e0217190fb45bd038145ea7ad301ed
kota_koyama <kouta103177@gmail.com>
2020-09-19 17:58:01 +0900

    ホストを指定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,15 +39,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
 
-  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yourdomain.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
