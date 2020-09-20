063985d8a3fca7d3c45e8d26428817c629e3dfdb
kota_koyama <kouta103177@gmail.com>
2020-09-20 15:17:30 +0900

    多言語化してみる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   def current_user
     ActiveDecorator::Decorator.instance.decorate(super) if super.present?
     super
   end
+
+  def default_url_options
+    #defaultのURL設定言語を変更
+    {locale: I18n.locale}
+  end
+  
+
+  private
+  def set_locale
+    #設定言語を変更する
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
+
