6c5527600fc42748ba3c2731500b49ad1baa61cc
kota_koyama <kouta103177@gmail.com>
2020-09-25 17:57:40 +0900

    categoryの修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@
 # Table name: articles
 #
 #  id         :bigint           not null, primary key
-#  category   :integer
+#  category   :integer          not null
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
