8c45aea9471ee281cc0145d30c7286e8b6e49a7d
kota_koyama <kouta103177@gmail.com>
2020-09-26 17:46:33 +0900

    article.js

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -22,8 +22,8 @@ const handleCommentForm = () => {
 
 const appendNewComment = (comment) => {
   $('.comments-container').append(
-    `<div class="article_comment"><p>${escape(comment.content)}</p></div>`
-  )
+    `<div class="article_comment"><p>${comment.content}</p></div>`	
+    )
 }
 
 document.addEventListener('DOMContentLoaded', () => {
